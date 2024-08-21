db7d42aef3de2dcbbf78a1503a0410a4af022c96
mattbaltzer <80438521+mattbaltzer@users.noreply.github.com>
2024-08-21 15:21:48 -0700

    Updated the ownership exercise with examples of reference, moving onto borrowing now

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -54,34 +54,61 @@
 //     println!("{some_integer}");
 // }   // Here some_integer goes out of scope. Nothing special happens
 
+// fn main() {
+//     // gives_ownership moves its return value into s1
+//     let s1 = gives_ownership();
+
+//     // s2 comes into scope
+//     let s2 = String::from("hello");
+
+//     // s2 is moved into takes_and_gives_back, which also moves
+//     // its return value into s3
+//     let s3 = takes_and_gives_back(s2);
+// } // Here, s3 goes out of scope and is dropped. s2 was moved
+// // so nothing happens. s1 goes out of scope and is dropped
+
+// // gives_ownership will move its return value into the function
+// // that calls it
+// fn gives_ownership() -> String {
+
+//     // some_string comes into scope
+//     let some_string = String::from("yours");
+
+//     // some_string is returned and moves out to the calling function
+//     some_string
+// }
+
+// // This function takes a String and returns one
+// fn takes_and_gives_back(a_string: String) -> String {
+//     // a_string comes into scope
+
+//     // a_string is returned and moves out to the calling function
+//     a_string
+// }
+
+// fn main() {
+//     let s1 = String::from("hello");
+
+//     let (s2, len) = calculate_length(s1);
+
+//     println!("The length of '{s2}' is {len}.");
+// }
+
+// fn calculate_length(s: String) -> (String, usize) {
+//     // len() returns the length of a string
+//     let length = s.len(); 
+
+//     (s, length)
+// }
+
 fn main() {
-    // gives_ownership moves its return value into s1
-    let s1 = gives_ownership();
+    let s1 = String::from("hello");
 
-    // s2 comes into scope
-    let s2 = String::from("hello");
+    let len = calculate_length(&s1);
 
-    // s2 is moved into takes_and_gives_back, which also moves
-    // its return value into s3
-    let s3 = takes_and_gives_back(s2);
-} // Here, s3 goes out of scope and is dropped. s2 was moved
-// so nothing happens. s1 goes out of scope and is dropped
-
-// gives_ownership will move its return value into the function
-// that calls it
-fn gives_ownership() -> String {
-
-    // some_string comes into scope
-    let some_string = String::from("yours");
-
-    // some_string is returned and moves out to the calling function
-    some_string
+    println!("The length of '{s1}' is {len}.");
 }
 
-// This function takes a String and returns one
-fn takes_and_gives_back(a_string: String) -> String {
-    // a_string comes into scope
-
-    // a_string is returned and moves out to the calling function
-    a_string
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
