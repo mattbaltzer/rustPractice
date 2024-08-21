3968db6498bf2707465242f8fc8fa08c382ea73f
mattbaltzer <80438521+mattbaltzer@users.noreply.github.com>
2024-08-21 08:51:52 -0700

    Added in an example of stack only data:copy. Ownership is rough and easily the biggest hurdle currently

---
+++ ownership/target/debug/deps/ownership-a05f5694739b660e.d
@@ -0,0 +1,5 @@
+D:\Programming\rustPractice\ownership\target\debug\deps\libownership-a05f5694739b660e.rmeta: src/main.rs
+
+D:\Programming\rustPractice\ownership\target\debug\deps\ownership-a05f5694739b660e.d: src/main.rs
+
+src/main.rs:
