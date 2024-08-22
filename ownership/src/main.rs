0c84970aa904d38ed00f5ea0d90ab513dfca0938
mattbaltzer <80438521+mattbaltzer@users.noreply.github.com>
2024-08-21 17:00:28 -0700

    Created an issue with immutable and mutable references in an attempt to better understand how they work with each other

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -125,11 +125,13 @@
 //     some_string.push_str(", world");
 // }
 
-fn main() {
-    let mut s = String::from("hello");
+// fn main() {
+//     let mut s = String::from("hello");
 
-    let r1 = &mut s;
-    let r2 = &mut s;
+//     let r1 = &s; // no problem
+//     let r2 = &s; // no problem
+//     let r3 = &mut s; // BIG PROBLEM
 
-    println!("{}, {}", r1, r2);
-}
+//     println!("{}, {}", r1, r2, r3);
+// }
+
