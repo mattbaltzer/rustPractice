3edd983496190b91a27e9ab0809799115dec086b
mattbaltzer <80438521+mattbaltzer@users.noreply.github.com>
2024-08-21 22:21:48 -0700

    Finished up borrowing exercise, going to start on slices tomorrow. Need to reread all of this because it isn't sinking in like I want it to

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -144,3 +144,15 @@
 
 //     &s
 // }
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
