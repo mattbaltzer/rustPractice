f815507802b3dc8b919c08a72e1eb46b746b373f
mattbaltzer <80438521+mattbaltzer@users.noreply.github.com>
2024-08-23 14:55:27 -0700

    Updated the struct with a struct update syntax shortcut that can be used

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,6 +5,24 @@ struct User {
     sign_in_count: u64,
 }
 
+// fn main() {
+//     let mut user1 = User {
+//         active: true,
+//         username: String::from("someusername123"),
+//         email: String::from("someone@eexample.com"),
+//         sign_in_count: 1,
+//     };
+
+//     let user2 = User {
+//         active: user1.active,
+//         username: user1.username,
+//         email: String::from("another@example.com"),
+//         sign_in_count: user1.sign_in_count,
+//     };
+
+//         user1.email = String::from("anotheremail@example.com");
+// }
+
 fn main() {
     let mut user1 = User {
         active: true,
@@ -13,14 +31,19 @@ fn main() {
         sign_in_count: 1,
     };
 
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
         user1.email = String::from("anotheremail@example.com");
 }
 
 fn build_user(email: String, username: String) -> User {
     User{
         active: true,
-        username,
-        email,
+        username, // shorthand because the struct and funtion have the same names for the key/value
+        email, // shorthand because the struct and funtion have the same names for the key/value
         sign_in_count: 1,
     }
 }
