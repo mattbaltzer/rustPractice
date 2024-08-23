347f9f5a51df7c34d8c4ba28080f28240dc89ad0
mattbaltzer <80438521+mattbaltzer@users.noreply.github.com>
2024-08-23 13:40:03 -0700

    Finished out ownership with borrowing and references, moved onto slices now. Have a basic slice example within here, doesn't work completely but am super close to figuring it out

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,6 +1,27 @@
-fn main() {
-    let s = String::from("hello world");
+// fn main() {
+//     let s = String::from("hello world");
 
-    let hello = &s[0..5];
-    let world = &s[6..11];
+//     let hello = &s[0..5];
+//     let world = &s[6..11];
+// }
+
+fn main() {
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    s.clear();
+
+    println!("The first word is: {word}");
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
---
+++ slices/target/debug/deps/slices-ef9d341d762f81ac.d
@@ -0,0 +1,5 @@
+D:\Programming\rustPractice\slices\target\debug\deps\libslices-ef9d341d762f81ac.rmeta: src/main.rs
+
+D:\Programming\rustPractice\slices\target\debug\deps\slices-ef9d341d762f81ac.d: src/main.rs
+
+src/main.rs:
