8482dc5fd2947627184d5c949c8d331540e4d8ad
mattbaltzer <80438521+mattbaltzer@users.noreply.github.com>
2024-08-23 14:41:45 -0700

    Created an example of a basic struct and how you'd utilize it. Found out some shorthand if the key/value pairs have the same name

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,10 +6,21 @@ struct User {
 }
 
 fn main() {
-    let user1 = User {
+    let mut user1 = User {
         active: true,
         username: String::from("someusername123"),
         email: String::from("someone@eexample.com"),
         sign_in_count: 1,
     };
+
+        user1.email = String::from("anotheremail@example.com");
+}
+
+fn build_user(email: String, username: String) -> User {
+    User{
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
 }
