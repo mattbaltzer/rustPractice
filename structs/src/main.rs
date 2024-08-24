0d385c3ad25260392b9eba3212541a00b23a83d0
mattbaltzer <80438521+mattbaltzer@users.noreply.github.com>
2024-08-23 18:02:42 -0700

    Updated the sturcts to have a unit-like struct example to show what kind of structs exist.

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,9 +1,9 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
+// struct User {
+//     active: bool,
+//     username: String,
+//     email: String,
+//     sign_in_count: u64,
+// }
 
 // fn main() {
 //     let mut user1 = User {
@@ -23,27 +23,41 @@ struct User {
 //         user1.email = String::from("anotheremail@example.com");
 // }
 
-fn main() {
-    let mut user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@eexample.com"),
-        sign_in_count: 1,
-    };
+// fn main() {
+//     let mut user1 = User {
+//         active: true,
+//         username: String::from("someusername123"),
+//         email: String::from("someone@eexample.com"),
+//         sign_in_count: 1,
+//     };
 
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
+//     let user2 = User {
+//         email: String::from("another@example.com"),
+//         ..user1
+//     };
 
-        user1.email = String::from("anotheremail@example.com");
+//         user1.email = String::from("anotheremail@example.com");
+// }
+
+// fn build_user(email: String, username: String) -> User {
+//     User{
+//         active: true,
+//         username, // shorthand because the struct and funtion have the same names for the key/value
+//         email, // shorthand because the struct and funtion have the same names for the key/value
+//         sign_in_count: 1,
+//     }
+// }
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+fn main(){
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
 
-fn build_user(email: String, username: String) -> User {
-    User{
-        active: true,
-        username, // shorthand because the struct and funtion have the same names for the key/value
-        email, // shorthand because the struct and funtion have the same names for the key/value
-        sign_in_count: 1,
-    }
+struct AlwaysEqual;
+
+fn main() {
+    let subject = AlwaysEqual;
 }
