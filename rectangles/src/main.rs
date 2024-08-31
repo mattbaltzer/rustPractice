f411c43ce73bc3e9f108476ca77d1990c542f8c9
mattbaltzer <80438521+mattbaltzer@users.noreply.github.com>
2024-08-31 16:48:23 -0700

    Added a new example for the area of a rectangle by using tuples

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,13 +1,29 @@
+
+//example without tuples
+// fn main() {
+//     let width1 = 30;
+//     let height1 = 50;
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(width1, height1)
+//     );
+// }
+
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+//example with tuples
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = (30, 50);
 
     println!(
-        "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        "The area of the rectangle is {} square pixels",
+        area(rect1)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
