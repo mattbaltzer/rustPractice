de9cf3fc607b2fafef00b53d45708f6a2c380dde
mattbaltzer <80438521+mattbaltzer@users.noreply.github.com>
2024-08-31 17:17:05 -0700

    Updated with an example for structs using inheritance and borrowing without moving

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -15,15 +15,41 @@
 // }
 
 //example with tuples
+//prone to errors with someone new introduced into our code
+// fn main() {
+//     let rect1 = (30, 50);
+
+//     println!(
+//         "The area of the rectangle is {} square pixels",
+//         area(rect1)
+//     );
+// }
+
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+//example with structs/refactoring with structs
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+//create an instance of a Rectangle struct with values
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 30,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels",
-        area(rect1)
+        area(&rect1)
     );
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+//accesses the width/height fields of the Rectangle instance
+//gives more clarity to the properties of the struct
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
