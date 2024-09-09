11c6c4f3afe41dda46ebf6b482afca1b046bd932
mattbaltzer <80438521+mattbaltzer@users.noreply.github.com>
2024-09-09 08:59:15 -0700

    Created a new rectangle struct with a newly created method.

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -71,19 +71,44 @@
 // }
 
 //better debug example using the dbg! macro
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let scale = 2;
+//     let rect1 = Rectangle {
+//         width: dbg!(30 * scale),
+//         height: 50,
+//     };
+
+//     //this prevents debug from taking ownership of rect1
+//     dbg!(&rect1);
+// }
+
+// creating a new macro
 #[derive(Debug)]
-struct Rectangle {
+struct  Rectangle {
     width: u32,
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
 
-    //this prevents debug from taking ownership of rect1
-    dbg!(&rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
