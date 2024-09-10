10ba9ad2fbade6212ff1118d40a7ec70343fb420
mattbaltzer <80438521+mattbaltzer@users.noreply.github.com>
2024-09-09 22:05:12 -0700

    Created a method showcasing multiple impl blocks

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -199,3 +199,34 @@
 //         println!("The rectangle has a nonzero width; it is {}", rect1.width);
 //     }
 // }
+
+// Using multiple impl blocks
+struct Rectangle {
+    width: u32, 
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+}
+
